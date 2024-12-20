4b8bf1be628b17484af017f4bc88a0b0a27c7ecc
preshenv <ndomaprecious95@gmail.com>
2024-12-20 11:20:11 +0100

    Update permit.js

--- backend/config/permit.js
+++ backend/config/permit.js
@@ -1,10 +1,8 @@
 // backend/config/permit.js
 const { Permit } = require("permitio");
-console.log(process.env.PERMIT_PDP_URL)
-console.log(process.env.PERMIT_API_KEY)
 const permit = new Permit({
-    pdp: process.env.PERMIT_PDP_URL || 'https://cloudpdp.api.permit.io',
-    token: process.env.PERMIT_API_KEY || 'permit_key_WPOcRMgGA6ovKTxYPps0zDmxbF7S185KA8092IBcMt71JpKEt48khrngmWn1bW6kljlbOrMxtJjueSLVHIIYM9',
+    pdp: process.env.PERMIT_PDP_URL',
+    token: process.env.PERMIT_API_KEY,
     debug: process.env.NODE_ENV !== 'production'
 });
 
